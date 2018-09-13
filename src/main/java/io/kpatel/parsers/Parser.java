4211554623a60e4d1bcff9fc3393a146a37d50df
Kishan Patel <kpatel20538@gmail.com>
2018-09-13 15:07:56 -0500

    API change, Removed Unintended String Requirement

--- src/main/java/io/kpatel/parsers/Parser.java
+++ src/main/java/io/kpatel/parsers/Parser.java
@@ -3,8 +3,7 @@ package io.kpatel.parsers;
 public interface Parser<T, Strm extends ParserStream<Strm,Seq,Itm>, Seq, Itm> {
     Result<T, Strm> parse(Strm stream);
 
-    default T parse(String text) {
-        // TODO: Implement Parser<T>.parse(String) -> ParseResult<T>
+    default T parse(Seq sequence) {
         return null;
     }
 }
